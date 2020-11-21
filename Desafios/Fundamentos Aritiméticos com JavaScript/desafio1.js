df6cfd202d3f151789da2b5021ba38367b81a628
MathSena <MathSena07@hotmail.com>
2020-11-21 00:02:32 -0300

    Finalizando Bootcamp

---
+++ Desafios/Fundamentos Aritiméticos com JavaScript/desafio1.js
@@ -0,0 +1,28 @@
+/*
+Desafio
+Crie um programa que leia 6 valores. Você poderá receber valores negativos e/ou positivos como entrada, devendo desconsiderar os valores nulos. Em seguida, apresente a quantidade de valores positivos digitados.
+
+Entrada
+Você receberá seis valores, negativos e/ou positivos.
+
+Saída
+Exiba uma mensagem dizendo quantos valores positivos foram lidos assim como é exibido abaixo no exemplo de saída. Não esqueça da mensagem "valores positivos" ao final.
+
+
+*/
+
+
+
+
+let valor1 = parseInt(gets());
+let valor2 = parseInt(gets());
+let valor3 = parseInt(gets());
+let valor4 = parseInt(gets());
+let valor5 = parseInt(gets());
+let valor6 = parseInt(gets());
+
+let array = [valor1,valor2,valor3,valor4,valor5,valor6];
+
+let pos = array.filter(value => value > 0);
+
+console.log(pos.length + " valores positivos");
